7a695c368a9c11c76cfda8136bcdab49bcf7d106
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-12 11:18:20 +0000

    vlink cjhanged to button in vid

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -30,7 +30,7 @@ function Videos() {
         <div><YouTube videoId="EcqKKUdlFCU" opts={opts} onReady={onPlayerReady} /></div>
       </div>
 
-      <a href=""><h1 className='text-white text-2xl lg:text-4xl text-center flex p-10 font-bold lg:ml-80 group/edit cursor-pointer'>Visit my channel<ImArrowUpRight2 className='text-gray-400 group-hover/edit:-translate-y-2.5 group-hover/edit:text-white duration-75 align-middle'/></h1></a>
+      <a href=""><button className='text-white text-2xl lg:text-4xl text-center flex p-10 font-bold lg:ml-80 group/edit cursor-pointer'>Visit my channel<ImArrowUpRight2 className='text-gray-400 group-hover/edit:-translate-y-2.5 group-hover/edit:text-white duration-75 align-middle'/></button></a>
 
     </div>
 
