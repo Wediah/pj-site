163e14fa86ccaa39d92624a2fa804e3ea28c2d82
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-12 21:19:49 +0000

    size reduced and centred in videos

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -10,8 +10,8 @@ function Videos() {
       }
     
       const opts: YouTubeProps['opts'] = {
-        height: '220',
-        width: '360',
+        height: '210',
+        width: '300',
         playerVars: {
           // https://developers.google.com/youtube/player_parameters
          
@@ -21,11 +21,11 @@ function Videos() {
 
   return (
     <Element id='videos' name='videos'>
-    <div className="bg-white min-h-screen px-10 pt-10 bg-no-repeat pb-5 bg-cover bg-center " style={{ backgroundImage: 'url(pastor4.png)'}}>
+    <div className="bg-white min-h-screen px-5 pt-10 bg-no-repeat pb-5 bg-cover bg-center " style={{ backgroundImage: 'url(pastor4.png)'}}>
 
       <h3 className=' text-4xl text-white font-bold pt-10 lg:text-center lg:text-7xl text-left '>Videos</h3>
 
-      <div className='flex flex-col gap-10 lg:flex-row lg:flex-wrap pt-16 lg:mx-80'>
+      <div className='flex flex-col gap-10 lg:flex-row lg:flex-wrap pt-16  object-center items-center'>
         <div><YouTube videoId="0zd9hbKlkvM" opts={opts} onReady={onPlayerReady} /></div>
         <div><YouTube videoId="Hl1XPdCX-2c" opts={opts} onReady={onPlayerReady} /></div>
         <div><YouTube videoId="yOffjjIIiqU" opts={opts} onReady={onPlayerReady} /></div>
