c16f4642720195feea8a94040bfb73d51528fdf9
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-12 00:20:15 +0000

    changes gray dept

--- pages/nav.tsx
+++ pages/nav.tsx
@@ -25,7 +25,7 @@ function Navbar () {
                                 smooth={true}
                                 offset={50}
                                 duration={500}
-                                className="cursor-pointer hover:text-cyan-600 text-white  px-3 py-2 rounded-md text-2xl font-extrabold "
+                                className="cursor-pointer hover:text-cyan-600 lg:text-white text-black  px-3 py-2 rounded-md text-2xl font-extrabold "
                             >
                                 PJ
                             </Link>
