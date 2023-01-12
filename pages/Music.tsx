bfc3812d85b838d11f160a698822683cc63071bf
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-12 10:06:42 +0000

    adds more png to public

--- pages/Music.tsx
+++ pages/Music.tsx
@@ -5,9 +5,12 @@ function Music() {
     <div className="bg-white min-h-screen px-10 pt-10 bg-no-repeat pb-5 bg-cover bg-center " style={{ backgroundImage: 'url(pastor4.png)'}}>
         <h1 className=' text-4xl text-white font-bold pt-10 lg:text-center lg:text-7xl text-left '>Music</h1>
 
+        <div>
+            <div></div>
+            <div></div>
+        </div>
 
 
-        
     </div>
   )
 }
