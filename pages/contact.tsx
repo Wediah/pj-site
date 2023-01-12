797bd4421d7208dd72f3f88cd02720885dff6c3d
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-12 11:00:13 +0000

    addds text to contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 function contact() {
   return (
-    <div>
-        
+    <div className="bg-white  px-10 pt-16 pb-32">
+
+    <h1 className=' text-4xl text-white font-bold pt-10 lg:text-7xl text-left '>Get In Touch With Me</h1>
+
+    <h2>send me a message</h2>
+    <h3>BOOK NOW</h3>
     </div>
   )
 }
