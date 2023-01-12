4b71566bae014e17300924608cb42b594b157fa7
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-12 09:51:53 +0000

    chnages to music page

---
+++ pages/Music.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Music() {
+  return (
+    <div className="bg-white min-h-screen px-10 pt-10 bg-no-repeat pb-5 bg-cover bg-center " style={{ backgroundImage: 'url(pastor4.png)'}}>
+        <h1 className=' text-4xl text-white font-bold pt-10 lg:text-center lg:text-7xl text-left '>Music</h1>
+
+
+
+        
+    </div>
+  )
+}
+
+export default Music
