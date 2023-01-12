e81df66c0f5fbbd378e1724cbd97b3ceb7be2a64
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-12 10:31:10 +0000

    adds icons to music page

--- pages/Music.tsx
+++ pages/Music.tsx
@@ -7,14 +7,14 @@ import pod from '../public/podcast.png'
 
 function Music() {
   return (
-    <div className="bg-white min-h-screen px-10 pt-10 bg-no-repeat pb-5 bg-cover bg-center " style={{ backgroundImage: 'url(pastor4.png)'}}>
-        <h1 className=' text-4xl text-white font-bold pt-10 lg:text-center lg:text-7xl text-left '>Music <span className='text-sm'>& Podcast</span></h1>
+    <div className="bg-white  px-10 pt-10 bg-no-repeat pb-10 bg-cover bg-center " style={{ backgroundImage: 'url(pastor4.png)'}}>
+        <h1 className=' text-4xl text-white font-bold pt-10 lg:text-center lg:text-7xl text-left '>Music <span className='text-2xl'>& Podcast</span></h1>
 
-        <div>
-            <div><Image src={apple} width={50} height={50} alt="" className="" /></div>
-            <div><Image src={spot} width={50} height={50} alt="" className="" /></div>
-            <div><Image src={itunes} width={50} height={50} alt="" className="" /></div>
-            <div><Image src={pod} width={50} height={50} alt="" className="" /></div>
+        <div className='flex gap-5 pt-16 mx-80'>
+            <div><Image src={apple} width={100} height={50} alt="" className="" /></div>
+            <div><Image src={spot} width={100} height={50} alt="" className="" /></div>
+            <div><Image src={itunes} width={100} height={50} alt="" className="" /></div>
+            <div><Image src={pod} width={100} height={50} alt="" className="" /></div>
         </div>
 
 
