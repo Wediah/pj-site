5400fbba3f3fd0c4c76656cb83b61b30138b944c
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-13 01:33:15 +0000

    adds blur to bg

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -22,18 +22,24 @@ function Videos() {
 
   return (
     <Element id='videos' name='videos'>
-    <div className="bg-white min-h-screen bg-fixed px-5 pt-10 bg-no-repeat pb-5 bg-cover bg-center " style={{ backgroundImage: 'url(pastor4.png)'}}>
+    <div className="bg-white  bg-fixed   bg-no-repeat  bg-cover bg-center " style={{ backgroundImage: 'url(pastor4.png)'}}>
 
-      <h3 className=' text-4xl text-white font-bold pt-10 lg:text-center lg:text-7xl text-left '>Videos</h3>
+      <div className='min-h-screen backdrop-blur-sm pb-5 pt-10 px-5'>
+        <h3 className=' text-4xl text-white font-bold pt-10 lg:text-center lg:text-7xl text-left '>Videos</h3>
 
-      <div className='flex flex-col gap-10 lg:flex-row lg:flex-wrap pt-16  object-center items-center'>
-        <div><YouTube videoId="0zd9hbKlkvM" opts={opts} onReady={onPlayerReady} /></div>
-        <div><YouTube videoId="Hl1XPdCX-2c" opts={opts} onReady={onPlayerReady} /></div>
-        <div><YouTube videoId="yOffjjIIiqU" opts={opts} onReady={onPlayerReady} /></div>
-        <div><YouTube videoId="EcqKKUdlFCU" opts={opts} onReady={onPlayerReady} /></div>
+        <div className='flex flex-col gap-10 lg:flex-row lg:flex-wrap pt-16  object-center items-center'>
+          <div><YouTube videoId="0zd9hbKlkvM" opts={opts} onReady={onPlayerReady} /></div>
+          <div><YouTube videoId="Hl1XPdCX-2c" opts={opts} onReady={onPlayerReady} /></div>
+          <div><YouTube videoId="yOffjjIIiqU" opts={opts} onReady={onPlayerReady} /></div>
+          <div><YouTube videoId="EcqKKUdlFCU" opts={opts} onReady={onPlayerReady} /></div>
+          <div><YouTube videoId="Hl1XPdCX-2c" opts={opts} onReady={onPlayerReady} /></div>
+          <div><YouTube videoId="yOffjjIIiqU" opts={opts} onReady={onPlayerReady} /></div>
+          <div><YouTube videoId="EcqKKUdlFCU" opts={opts} onReady={onPlayerReady} /></div>
+        </div>
+
+        <a href=""><button className='text-white text-2xl lg:text-4xl text-center flex p-10 font-bold lg:ml-96 group/edit cursor-pointer items-center '>Visit my channel<ImArrowUpRight2 className='text-gray-400 group-hover/edit:-translate-y-2.5 group-hover/edit:text-white duration-75 align-middle'/></button></a>
       </div>
 
-      <a href=""><button className='text-white text-2xl lg:text-4xl text-center flex p-10 font-bold lg:ml-80 group/edit cursor-pointer'>Visit my channel<ImArrowUpRight2 className='text-gray-400 group-hover/edit:-translate-y-2.5 group-hover/edit:text-white duration-75 align-middle'/></button></a>
 
     </div>
     </Element>
