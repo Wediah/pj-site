a88d28a4031a43a675e5a54beb40aacb9c306b87
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-13 12:14:30 +0000

    adds padding to contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -9,7 +9,7 @@ function Contact() {
     <h1 className=' text-4xl text-black font-bold pt-10 pb-5 lg:text-6xl text-left '>Get In Touch With Me</h1>
 
     <h2 className='text-md font-thin pb-2'>send me a message</h2>
-    <a href="https://wa.me/+233242508586"><button className='p-1 bg-black text-white text-md rounded-sm'>BOOK NOW</button></a>
+    <a href="https://wa.me/+233242508586"><button className='p-2 bg-black text-white text-md rounded-sm'>BOOK NOW</button></a>
     </div>
     </Element>
   )
