8c600b0302a99c0aa72adba8dad8c0cf7f44fcf1
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-13 03:30:27 +0000

    links added in music

--- pages/Music.tsx
+++ pages/Music.tsx
@@ -14,10 +14,10 @@ function Music() {
         <h1 className=' text-4xl text-white font-bold pt-10 lg:text-center lg:text-7xl text-left '>Music <span className='text-2xl'>& Podcast</span></h1>
 
         <div className='flex gap-5 pt-16  justify-center'>
-            <div><Image src={apple} width={100} height={50} alt="" className="" /></div>
-            <div><Image src={spot} width={100} height={50} alt="" className="" /></div>
-            <div><Image src={itunes} width={100} height={50} alt="" className="" /></div>
-            <div><Image src={pod} width={100} height={50} alt="" className="" /></div>
+            <a href="https://music.apple.com/gh/artist/jennifer-kofi/1471115987"><div><Image src={apple} width={100} height={50} alt="" className="" /></div></a>
+            <a href="https://open.spotify.com/artist/7CAKn7NVm6DUR3w6yGvPUU"><div><Image src={spot} width={100} height={50} alt="" className="" /></div></a>
+            <a href=""><div><Image src={itunes} width={100} height={50} alt="" className="" /></div></a>
+            <a href="https://podcasts.apple.com/us/podcast/the-revive-podcast/id1611004724"><div><Image src={pod} width={100} height={50} alt="" className="" /></div></a>
         </div>
       </div>
 
