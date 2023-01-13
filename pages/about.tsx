29fe7718b7615b449be38542954d8551fb0bb64b
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-13 01:19:06 +0000

    adds blur overlay to about

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,13 +6,13 @@ import Image from 'next/image'
 function About () {
     return (
         <Element id="about" name="about">
-            <div className="bg-white min-h-screen px-10  bg-no-repeat  bg-fixed bg-cover bg-center" style={{ backgroundImage: 'url(pastor2.jpg)'}}>
+            <div className="bg-white  px-10  bg-no-repeat  bg-fixed bg-cover bg-center" style={{ backgroundImage: 'url(pastor2.jpg)'}}>
                 
-                <div className=' backdrop-blur-sm '>
+                <div className='min-h-screen backdrop-blur-sm '>
                 <h3 className=' text-4xl text-white font-bold pt-20 lg:text-center lg:text-7xl text-left '>About Me</h3>
                 
                     
-                <div className=' lg:px-16 gap-20 lg:flex '>      
+                <div className=' lg:px-16 gap-20 lg:flex lg:pt-20'>      
                     
                     <div className="text-2xl lg:text-4xl flex text-white  leading-8 pt-20 pb-10 ">
                     Reverend Jennifer Kofi is a Ghanaian worshiper, preacher and songwriter, whose vocation is to worship in spirit and truth and also point the world to Christ.  <br /> Rev. Jennifer has ministered across the Ghana and sharing the stage with the likes of Bethel Revival Music and Joe Mettle. <br /> In 2022, She also appeared on Bethel Revival Choir&aposs ENYO album.
