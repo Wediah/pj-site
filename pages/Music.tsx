ff8aff682cd5525decd5ded928105804427536e5
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-13 02:09:44 +0000

    adds justification to music icons

--- pages/Music.tsx
+++ pages/Music.tsx
@@ -13,7 +13,7 @@ function Music() {
       <div className='backdrop-blur-sm px-10 pt-16 pb-32'>
         <h1 className=' text-4xl text-white font-bold pt-10 lg:text-center lg:text-7xl text-left '>Music <span className='text-2xl'>& Podcast</span></h1>
 
-        <div className='flex gap-5 pt-16 lg:mx-96'>
+        <div className='flex gap-5 pt-16  justify-center'>
             <div><Image src={apple} width={100} height={50} alt="" className="" /></div>
             <div><Image src={spot} width={100} height={50} alt="" className="" /></div>
             <div><Image src={itunes} width={100} height={50} alt="" className="" /></div>
